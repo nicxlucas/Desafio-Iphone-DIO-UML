456c28367f107990c8ca71fdb4c328d8489a3297
Lucas Nicolau <lucasnicolauprf@gmail.com>
2024-05-27 11:23:51 -0300

    Realizando Desafio de Criação UML da DIO.me

---
+++ Iphone.java
@@ -0,0 +1,52 @@
+
+public class Iphone implements ReprodutorMusical,AparelhoTelefonico,NavegadorInternet{
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo a pagina " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("pagina Atualizada");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("tocando");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a musica " + musica);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Desafio-iphone-dio-uml
 Reposítorio criado para desafio teste da DIO.me (criação de UML de um iphone)
+
+classDiagram
+
+    class ReprodutorMusical {
+    +tocar()
+    +pausar(String exemplo)
+    +selecionarMusica(String musica)
+    }
+    class AparelhoTelefonico {
+        +ligar(String numero)
+        +atender()
+        +iniciarCorreioVoz()
+    }
+    class NavegadorInternet {
+        +exibirPagina(String url)
+        +adicionarNovaAba()
+        +atualizarPagina()
+    }
+    class iPhone {
+    }
+    iPhone --> ReprodutorMusical
+    iPhone --> AparelhoTelefonico
+    iPhone --> NavegadorInternet
